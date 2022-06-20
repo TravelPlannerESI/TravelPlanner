a8005abaeaac61e9fcd4616e20d4d543a4b584a3
Subin Cho <sbjo7713@gmail.com>
2022-06-20 17:17:47 +0900

    User table 명 수정, build.gradle 수정

--- src/main/java/com/travelplan/travel/domain/User.java
+++ src/main/java/com/travelplan/travel/domain/User.java
@@ -1,12 +1,17 @@
 package com.travelplan.travel.domain;
 
 import com.travelplan.travel.domain.code.UserRole;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.lang.annotation.Target;
 
 @Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "USERS")
 public class User {
 
     @Id
@@ -20,6 +25,13 @@ public class User {
     @Enumerated(EnumType.STRING)
     private UserRole userRole;
 
-//    @OneToMany(mappedBy = "user")
+    public User(String userName, String email, String userPicture, UserRole userRole) {
+        this.userName = userName;
+        this.email = email;
+        this.userPicture = userPicture;
+        this.userRole = userRole;
+    }
+
+    //    @OneToMany(mappedBy = "user")
 //    private List<Member> members = new ArrayList<>();
 }
